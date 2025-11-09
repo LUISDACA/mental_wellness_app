575d10ec847c2759aff8d6a3f793b69e9eb59412
juanHD269 <juan.hernandezde@campusucc.edu.co>
2025-11-09 01:27:02 -0500

    67 commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'data/services/supabase_client.dart';
 import 'presentation/app.dart';
+import 'core/error_handler.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  AppErrorHandler.init();
   await Supa.init();
   runApp(const ProviderScope(child: App()));
 }
